b3c7205eb289f12e10e38ba389ed730d252c22e3
choi5611 <157350@naver.com>
2024-05-20 20:50:10 +0900

    first commit

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -3,45 +3,37 @@ import { useRouter } from "next/router";
 import React, { FormEvent, useState } from "react";
 import InputGroup from "../components/InputGroup";
 import axios from "axios";
-// import { useAuthState } from "../context/auth";
-
-const Register = () => {
-  const [email, setEmail] = useState("");
+const Login = () => {
+  let router = useRouter();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState<any>({});
-  //   const { authenticated } = useAuthState();
-
-  let router = useRouter();
-  //   if (authenticated) router.push("/");
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
     try {
-      const res = await axios.post("/auth/register", {
-        email,
-        password,
-        username,
-      });
-      console.log("res", res);
-      router.push("/login");
+      const res = await axios.post(
+        "/auth/login",
+        {
+          password,
+          username,
+        },
+        {
+          withCredentials: true,
+        }
+      );
     } catch (error: any) {
-      console.log("error", error);
+      console.log("뭐지", error);
       setErrors(error.response.data || {});
     }
   };
+
   return (
     <div className="bg-white">
       <div className="flex flex-col items-center justify-center h-screen p-6">
         <div className="w-10/12 mx-auto md:w-96">
-          <h1 className="mb-2 text-lg font-medium">회원가입</h1>
+          <h1 className="mb-2 text-lg font-medium">로그인</h1>
           <form onSubmit={handleSubmit}>
-            <InputGroup
-              placeholder="Email"
-              value={email}
-              setValue={setEmail}
-              error={errors.email}
-            />
             <InputGroup
               placeholder="Username"
               value={username}
@@ -55,13 +47,13 @@ const Register = () => {
               error={errors.password}
             />
             <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
-              회원 가입
+              로그인
             </button>
           </form>
           <small>
-            이미 가입하셨나요?
-            <Link href="/login" legacyBehavior>
-              <a className="ml-1 text-blue-500 uppercase">로그인</a>
+            아직 아이디가 없나요?
+            <Link href="/register" legacyBehavior>
+              <a className="ml-1 text-blue-500 uppercase">회원가입</a>
             </Link>
           </small>
         </div>
@@ -70,4 +62,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
