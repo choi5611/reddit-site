30722e90271360a47e86ada1159cdd154ad44bf5
choi5611 <157350@naver.com>
2024-05-26 01:14:42 +0900

    first commit

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -26,7 +26,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <script></script>
+        <script
+          defer
+          src="https://use.fontawesome.com/releases/v6.1.1/js/all.js"
+          integrity="sha384-xBXmu0dk1bEoiwd71wOonQLyH+VpgR1XcDH3rtxrLww5ajNTuMvBdL5SOiFZnNdp"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <SWRConfig
         value={{
