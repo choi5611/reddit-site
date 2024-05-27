b637074069b59c2faf1400b6636ae60891175ccd
choi5611 <157350@naver.com>
2024-05-28 05:46:59 +0900

    first commit

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,22 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.gravatar.com",
-        pathname: "**", // 모든 경로 허용
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        pathname: "**", // 모든 경로 허용
-      },
-      {
-        protocol: "http",
-        hostname: "ec2-3-34-91-234.ap-northeast-2.compute.amazonaws.com",
-        pathname: "**", // 모든 경로 허용
-      },
+    domains: [
+      "www.gravatar.com",
+      "localhost",
+      "ec2-3-34-91-234.ap-northeast-2.compute.amazonaws.com",
     ],
   },
 };
